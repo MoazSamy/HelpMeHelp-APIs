7c9e382981069142146df64244dbce07e43f6e8b
Moaz Samy <moazsamy333@gmail.com>
2022-05-13 15:25:16 +0200

    Adding email to user model

--- helpmehelp/users/models.py
+++ helpmehelp/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -13,6 +14,7 @@ class User(AbstractUser):
 
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    email = models.EmailField(_("Email"))
     first_name = None  # type: ignore
     last_name = None  # type: ignore
 
