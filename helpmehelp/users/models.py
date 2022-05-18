9303094f9b3b6a33b7e69a6e6055a84ac0adfe26
Moaz Samy <moazsamy333@gmail.com>
2022-05-18 15:13:45 +0200

    Modifying user model

--- helpmehelp/users/models.py
+++ helpmehelp/users/models.py
@@ -1,28 +1,45 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models import CharField
 from django.urls import reverse
+from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    """
-    Default custom user model for HelpMeHelp.
-    If adding fields that need to be filled at user signup,
-    check forms.SignupForm and forms.SocialSignupForms accordingly.
-    """
+    blood_types = [
+        ("O+", "O Positive"),
+        ("O-", "O Negative"),
+        ("A+", "A Positive"),
+        ("A-", "A Negative"),
+        ("B+", "B Positive"),
+        ("B-", "B Negative"),
+        ("AB+", "AB Positive"),
+        ("AB-", "AB Negative"),
+    ]
+    name = models.CharField(_("User Full Name"), max_length=155)
+    username = models.CharField(_("Username"), max_length=155, unique=True)
+    email = models.EmailField(_("Email"), max_length=155, unique=True)
+    age = models.PositiveSmallIntegerField(null=True)
+    phone = models.CharField(_("Phone Number"), max_length=12, null=True)
+    blood = models.CharField(
+        _("Blood Type"), max_length=3, choices=blood_types, null=True
+    )
+    address = models.CharField(_("User Address"), max_length=200, null=True)
+    nat_ID = models.CharField(
+        _("National ID"), max_length=14, unique=True, null=True, blank=True
+    )
+    is_verified = models.BooleanField(_("Is user verified by email"), default=False)
+    date_joined = models.DateTimeField(default=now)
 
-    #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    email = models.EmailField(_("Email"))
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
+    first_name = None
+    last_name = None
 
     def get_absolute_url(self):
         """Get url for user's detail view.
-
         Returns:
             str: URL for user detail.
-
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+    def __str__(self):
+        return self.username
