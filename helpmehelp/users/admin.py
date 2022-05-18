bf3d88ad4375714bd435bbeae8ec284d3aa57885
Moaz Samy <moazsamy333@gmail.com>
2022-05-18 15:12:53 +0200

    Editing admin view

--- helpmehelp/users/admin.py
+++ helpmehelp/users/admin.py
@@ -15,20 +15,17 @@ class UserAdmin(auth_admin.UserAdmin):
     add_form = UserAdminCreationForm
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("name", "email")}),
+        (
+            _("Personal info"),
+            {"fields": ("name", "email", "age", "phone", "blood", "address", "nat_ID")},
+        ),
         (
             _("Permissions"),
             {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                ),
+                "fields": ("is_verified",),
             },
         ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
-    list_display = ["username", "name", "is_superuser"]
+    list_display = ["username", "name", "is_verified"]
     search_fields = ["name"]
