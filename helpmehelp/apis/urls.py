c8cf2577fa979bd73e271f0c48fe66e1defdbef8
Moaz Samy <moazsamy333@gmail.com>
2022-05-20 15:20:00 +0200

    Adding apis app to manage apps urls

---
+++ helpmehelp/apis/urls.py
@@ -0,0 +1,7 @@
+from django.urls import include, path
+
+app_name = "apis"
+urlpatterns = [
+    path("auth/", include("helpmehelp.authentication.urls", "authentication")),
+    path("users/", include("helpmehelp.users.urls", "users")),
+]
